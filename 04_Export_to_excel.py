02084e7c737fd344574e694da08168b3495ef80c
Itay Manes <itaymanes@gmail.com>
2019-02-27 23:37:05 +0200

    Update 04_Export_to_excel.py

--- 04_Export_to_excel.py
+++ 04_Export_to_excel.py
@@ -18,15 +18,8 @@ imageDataWriter = csv.writer(imageData)
 
 cluster_final = {}
 for k in sorted(cluster_to_words, key=lambda k: len(cluster_to_words[k]), reverse=True):
-    #temp_list = cluster_to_words[k]
     for ind, x in enumerate(cluster_to_words[k]):
         a = x.encode("ascii")
         cluster_to_words[k][ind] = bytes.decode(a)
-            #temp_list[ind] = x.encode("utf-8")
-            #print(type(x))
-            #cluster_to_words[k][ind] = bytes.decode(x)
-            #temp_list[ind] = x.encode("utf-8").decode()
-            #temp_list[ind] = bytes(x, "utf-8")
-    #cluster_final[k] = (cluster_to_words[k])
     imageDataWriter.writerow(cluster_to_words[k])
 imageData.close()
